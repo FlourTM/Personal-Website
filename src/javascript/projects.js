a7e46e0ce50af578e3e78e5b298b806c914ece63
FlourTM <gabriellag726@gmail.com>
2023-12-03 00:37:34 -0500

    Updated Projects page with descriptions and images

---
+++ src/javascript/projects.js
@@ -0,0 +1,97 @@
+const projectImages = {
+    1: [
+        '../assets/projectImages/knowledgeGraph/image1.png',
+        '../assets/projectImages/knowledgeGraph/image2.png',
+        '../assets/projectImages/knowledgeGraph/image3.png',
+    ],
+    2: [
+        '../assets/projectImages/ecommerce/image1.png',
+        '../assets/projectImages/ecommerce/image2.png',
+        '../assets/projectImages/ecommerce/image3.png',
+    ],
+    3: [
+        '../assets/projectImages/recipeSaver/image1.png',
+        '../assets/projectImages/recipeSaver/image2.png',
+        '../assets/projectImages/recipeSaver/image3.png',
+        '../assets/projectImages/recipeSaver/image4.png',
+    ],
+}
+
+let currentImage = 0
+
+function toggleSlideshow(projectNumber) {
+    const modal = document.getElementById(`slideshowModal${projectNumber}`)
+    modal.classList.toggle('hidden')
+    currentImage = 0
+    updateSlideshowImages(projectNumber)
+}
+
+function closeSlideshow() {
+    const modals = document.querySelectorAll('.overlay')
+    modals.forEach(function (modal) {
+        modal.classList.add('hidden')
+    })
+}
+
+function navigateSlideshow(direction) {
+    const activeModal = document.querySelector('.overlay:not(.hidden)');
+    const projectNumber = activeModal.id.replace('slideshowModal', '');
+
+    currentImage += direction;
+
+    // Log values for debugging
+    console.log('projectNumber:', projectNumber);
+    console.log('currentImage:', currentImage);
+    console.log('projectImages[projectNumber]:', projectImages[projectNumber]);
+
+    // Ensure the index stays within bounds
+    if (currentImage < 0) {
+        currentImage = projectImages[projectNumber].length - 1;
+    } else if (currentImage >= projectImages[projectNumber].length) {
+        currentImage = 0;
+    }
+
+    updateSlideshowImages(projectNumber);
+}
+
+function updateSlideshowImages(projectNumber) {
+    const slideshowImages = document.querySelector(`#slideshowModal${projectNumber} .slideshow-images`);
+    const newImage = document.createElement('img');
+    newImage.src = projectImages[projectNumber][currentImage];
+    newImage.classList.add('fade-in');
+    
+    // Remove existing images before appending the new one
+    while (slideshowImages.firstChild) {
+        slideshowImages.removeChild(slideshowImages.firstChild);
+    }
+    
+    // Append the new image
+    slideshowImages.appendChild(newImage);
+
+    // Add a counter element to display the current image number
+    const counterElement = document.querySelector(`#slideshowModal${projectNumber} .image-counter`);
+    counterElement.textContent = `${currentImage + 1} / ${projectImages[projectNumber].length}`;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.slideshowBtns')
+    buttons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            toggleSlideshow(index + 1)
+        })
+    })
+
+    const closeButtons = document.querySelectorAll('.close')
+    closeButtons.forEach(function (closeButton) {
+        closeButton.addEventListener('click', function () {
+            closeSlideshow()
+        })
+    })
+
+    const arrowButtons = document.querySelectorAll('.arrow')
+    arrowButtons.forEach(function (arrowButton, index) {
+        arrowButton.addEventListener('click', function () {
+            navigateSlideshow(parseInt(arrowButton.dataset.direction, 10));
+        })
+    })
+})
