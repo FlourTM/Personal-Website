566826387e3a080569da2d9dafd26672a1b64994
FlourTM <gabriellag726@gmail.com>
2023-12-03 13:24:05 -0500

    added images for ecommerce project

--- src/javascript/projects.js
+++ src/javascript/projects.js
@@ -8,6 +8,15 @@ const projectImages = {
         '../assets/projectImages/ecommerce/image1.png',
         '../assets/projectImages/ecommerce/image2.png',
         '../assets/projectImages/ecommerce/image3.png',
+        '../assets/projectImages/ecommerce/image4.png',
+        '../assets/projectImages/ecommerce/image5.png',
+        '../assets/projectImages/ecommerce/image6.png',
+        '../assets/projectImages/ecommerce/image7.png',
+        '../assets/projectImages/ecommerce/image8.png',
+        '../assets/projectImages/ecommerce/image9.png',
+        '../assets/projectImages/ecommerce/image10.png',
+        '../assets/projectImages/ecommerce/image11.png',
+        '../assets/projectImages/ecommerce/image12.png',
     ],
     3: [
         '../assets/projectImages/recipeSaver/image1.png',
@@ -34,43 +43,38 @@ function closeSlideshow() {
 }
 
 function navigateSlideshow(direction) {
-    const activeModal = document.querySelector('.overlay:not(.hidden)');
-    const projectNumber = activeModal.id.replace('slideshowModal', '');
+    const activeModal = document.querySelector('.overlay:not(.hidden)')
+    const projectNumber = activeModal.id.replace('slideshowModal', '')
 
-    currentImage += direction;
-
-    // Log values for debugging
-    console.log('projectNumber:', projectNumber);
-    console.log('currentImage:', currentImage);
-    console.log('projectImages[projectNumber]:', projectImages[projectNumber]);
+    currentImage += direction
 
     // Ensure the index stays within bounds
     if (currentImage < 0) {
-        currentImage = projectImages[projectNumber].length - 1;
+        currentImage = projectImages[projectNumber].length - 1
     } else if (currentImage >= projectImages[projectNumber].length) {
-        currentImage = 0;
+        currentImage = 0
     }
 
-    updateSlideshowImages(projectNumber);
+    updateSlideshowImages(projectNumber)
 }
 
 function updateSlideshowImages(projectNumber) {
-    const slideshowImages = document.querySelector(`#slideshowModal${projectNumber} .slideshow-images`);
-    const newImage = document.createElement('img');
-    newImage.src = projectImages[projectNumber][currentImage];
-    newImage.classList.add('fade-in');
+    const slideshowImages = document.querySelector(`#slideshowModal${projectNumber} .slideshow-images`)
+    const newImage = document.createElement('img')
+    newImage.src = projectImages[projectNumber][currentImage]
+    newImage.classList.add('fade-in')
     
     // Remove existing images before appending the new one
     while (slideshowImages.firstChild) {
-        slideshowImages.removeChild(slideshowImages.firstChild);
+        slideshowImages.removeChild(slideshowImages.firstChild)
     }
     
     // Append the new image
-    slideshowImages.appendChild(newImage);
+    slideshowImages.appendChild(newImage)
 
     // Add a counter element to display the current image number
-    const counterElement = document.querySelector(`#slideshowModal${projectNumber} .image-counter`);
-    counterElement.textContent = `${currentImage + 1} / ${projectImages[projectNumber].length}`;
+    const counterElement = document.querySelector(`#slideshowModal${projectNumber} .image-counter`)
+    counterElement.textContent = `${currentImage + 1} / ${projectImages[projectNumber].length}`
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -91,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const arrowButtons = document.querySelectorAll('.arrow')
     arrowButtons.forEach(function (arrowButton, index) {
         arrowButton.addEventListener('click', function () {
-            navigateSlideshow(parseInt(arrowButton.dataset.direction, 10));
+            navigateSlideshow(parseInt(arrowButton.dataset.direction, 10))
         })
     })
 })
